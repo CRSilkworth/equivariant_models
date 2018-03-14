a8c80f4bf3f0a1abd3a9c9b4975a997074cdceb3
CRSilkworth <csilkworth@locus.co>
2018-03-14 17:58:43 +0800

    added cfgs

---
+++ alex_net/cfgs/eval_cfg.py
@@ -0,0 +1,8 @@
+
+
+url_file_name = '/Users/CRSilkworth/Downloads/fall11_urls.txt'
+img_dir = './images/'
+allowed_extensions = ['jpg', 'jpeg']
+image_size = (227, 227)
+label_file_name = '../image_net/all_labels.txt'
+lines_to_skip_file_name = 'lines_to_skip.txt'
