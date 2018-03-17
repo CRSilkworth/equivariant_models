169e928e1c1757b0c15c6230a68b0e67bf258aeb
CRSilkworth <csilkworth@locus.co>
2018-03-18 07:25:15 +0800

    added cfgs

---
+++ image_net/cfgs/write_images_to_tfrecords_cfg.py
@@ -0,0 +1,9 @@
+
+train_directory = './images/train'
+validation_directory = './images/val'
+label_defs_file_name = './labels/label_defs.txt'
+val_labels_file_name = './labels/val_labels.txt'
+output_directory = './tf_records'
+train_shards = 2
+validation_shards = 2
+num_threads = 2
