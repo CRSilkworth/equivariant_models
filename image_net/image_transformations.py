0cb640774e030ca87dafae7f41d6899673f52517
Christopher Silkworth <csilkworth@locus.co>
2018-04-07 22:17:35 -0400

    5 crop and flip working

--- image_net/image_transformations.py
+++ image_net/image_transformations.py
@@ -3,13 +3,14 @@
 """Data augmenting and standard processing transformations performed on images."""
 import tensorflow as tf
 
-
 def crop_to_center(image, image_size, crop_image_size):
     """Take the center crop of an image of size image_size down to crop_size crop_image_size. If the the size of a dimension is odd then it takes the slightly shifted left or up."""
+
     crop_h = (image_size[0] - crop_image_size[0])/2
     crop_w = (image_size[1] - crop_image_size[1])/2
 
     image = image[crop_h: crop_h + crop_image_size[0], crop_w: crop_w + crop_image_size[1], :]
+
     return image
 
 
@@ -45,23 +46,27 @@ def rgb_distortion(image, rgb_eigenvectors, rgb_eigenvalues, stddev=0.1, to_run=
 def add_horizontal_flip_to_batch(images, labels, to_run=None):
     # Flip along the width axis, stack with the original along the batch dim
     # and return.
-    flipped = tf.reverse(images, axis=2)
+    flipped = tf.reverse(images, axis=[2])
     labels = tf.tile(labels, multiples=[2])
-    return tf.stack([images, flipped], axis=0), labels
+    stacked = tf.concat([images, flipped], axis=0)
+    
+    return stacked, labels
 
 
-def add_five_crops_to_batch(images, labels, crop_image_size, to_run=None):
+def add_five_crops_to_batch(images, labels, image_size, crop_image_size, to_run=None):
     # Four corner crops
     crop_1 = images[:, :crop_image_size[0], :crop_image_size[1], :]
     crop_2 = images[:, -crop_image_size[0]:, :crop_image_size[1], :]
-    crop_3 = images[:, :crop_image_size[0], :-crop_image_size[1], :]
-    crop_4 = images[:, -crop_image_size[0]:, :-crop_image_size[1], :]
+    crop_3 = images[:, :crop_image_size[0], -crop_image_size[1]:, :]
+    crop_4 = images[:, -crop_image_size[0]:, -crop_image_size[1]:, :]
 
     # Center crop
-    crop_5 = tf.image.resize_image_with_crop_or_pad(images, crop_image_size[0], crop_image_size[1])
+    crop_h = (image_size[0] - crop_image_size[0])/2
+    crop_w = (image_size[1] - crop_image_size[1])/2
+    crop_5 = images[:, crop_h: crop_h + crop_image_size[0], crop_w: crop_w + crop_image_size[1], :]
 
     # Stack them along the batch dim and return.
-    concats = tf.stack(
+    concats = tf.concat(
         [crop_1, crop_2, crop_3, crop_4, crop_5],
         axis=0
     )
